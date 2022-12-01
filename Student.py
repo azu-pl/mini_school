5171ad5caaaad0b2db5531e0b7964de05f81b86f
dados32 <83305546+dados32@users.noreply.github.com>
2022-12-01 07:33:10 +0100

    Update Student.py

--- Student.py
+++ Student.py
@@ -1,9 +1,1 @@
-from Person import Person
-
-
-class Student(Person):
-    def __init__(self, person_id: int, first_name: str, last_name: str, sex: str, student_id: int, index_number: int):
-
-        super().__init__(person_id, first_name, last_name, sex)
-        self.student_id = student_id
-        self.index_number = index_number
+#
